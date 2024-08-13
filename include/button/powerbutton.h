2fa7caf9b42da73887a68486d4adb94792995fbc
Gyorgy Sarvari <skandigraun@gmail.com>
2024-08-13 17:16:16 +0200

    add power button config for pinephone
    PinePhone and PinePhonePro have different power button names.
    
    This commit adds two options to the CMake config (pinephone and pinephone_pro),
    which determines for which device it should be built.
    
    NB for self: should I just move it to a config with SettingsLib...?

--- include/button/powerbutton.h
+++ include/button/powerbutton.h
@@ -6,7 +6,13 @@
 #define POWER_BUTTON_PRESS      "powerButtonPress"
 #define POWER_BUTTON_RELEASE    "powerButtonRelease"
 
+#ifdef PINEPHONE_PRO
 #define POWER_BUTTON_PHYS  "gpio-keys/input0"
+#elif defined PINEPHONE
+#define POWER_BUTTON_PHYS  "m1kbd/input2"
+#else
+static_assert(false, "Either PINEPHONE or PINEPHONE_PRO macro must be defined! Check Cmake config!");
+#endif
 
 class PowerButton
 {
